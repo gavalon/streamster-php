8cbb4deaf89ce5e061d9e849e8527be201efa249
gavalon <gavalon@stanford.edu>
2016-02-13 23:55:32 -0800

    I screwed up

--- single_video.php
+++ single_video.php
@@ -9,8 +9,8 @@ const CLIENT_ID     = 'df0b16a519f13cb01d36ce0b0233d620074f789242f3aec51bebbb0f4
 const RESPONSE_TYPE = 'code';
 const CLIENT_SECRET = '3b6e4af36d658a2347381db8c554ff8d38ad8595112c2eb2b9f249850fbabaec';
 
-//const REDIRECT_URI           = 'http://streamsterphp.azurewebsites.net/single_video.php';
-const REDIRECT_URI           = 'http://localhost/streamster-php/single_video.php';
+const REDIRECT_URI           = 'http://streamsterphp.azurewebsites.net/single_video.php';
+//const REDIRECT_URI           = 'http://localhost/streamster-php/single_video.php';
 const AUTHORIZATION_ENDPOINT = 'https://www.coinbase.com/oauth/authorize';
 const TOKEN_ENDPOINT         = 'https://www.coinbase.com/oauth/token';
 
