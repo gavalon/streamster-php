6d8c85558f675e8d54eb14e62a2189cf89fcd753
gavalon <gavalon@stanford.edu>
2016-02-13 23:54:20 -0800

    tests inconclusive

--- single_video.php
+++ single_video.php
@@ -9,8 +9,8 @@ const CLIENT_ID     = 'df0b16a519f13cb01d36ce0b0233d620074f789242f3aec51bebbb0f4
 const RESPONSE_TYPE = 'code';
 const CLIENT_SECRET = '3b6e4af36d658a2347381db8c554ff8d38ad8595112c2eb2b9f249850fbabaec';
 
-const REDIRECT_URI           = 'http://streamsterphp.azurewebsites.net/single_video.php';
-//const REDIRECT_URI           = 'http://localhost/streamster-php/single_video.php';
+//const REDIRECT_URI           = 'http://streamsterphp.azurewebsites.net/single_video.php';
+const REDIRECT_URI           = 'http://localhost/streamster-php/single_video.php';
 const AUTHORIZATION_ENDPOINT = 'https://www.coinbase.com/oauth/authorize';
 const TOKEN_ENDPOINT         = 'https://www.coinbase.com/oauth/token';
 
@@ -123,5 +123,6 @@ $client = Client::create($configuration);
    <div id="current-time-spent"></div>
    <div id="views-info"></div>
    <!-- <p>bitcoin</p> -->
+   <p><?php echo gettype($response) ?></p>
    <p><?php echo $response ?></p>
 </body>
