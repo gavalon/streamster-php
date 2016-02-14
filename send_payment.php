1c484fc87cd5fe0f662d3e8a9f1e39fefd781cc6
gavalon <gavalon@stanford.edu>
2016-02-14 11:59:23 -0800

    Token

--- send_payment.php
+++ send_payment.php
@@ -1,5 +1,5 @@
 <?php
-$accessToken = '58421a5dbec5fb71e741ac21b67b4ebf551d099ebfca2fa66c0201890b982756';
+$accessToken = '11c7dd3ecc51aee5ad330dfce589a6df779d33bce32d871336dd526c6a396db9';
 $account_id = 'ea3df725-5698-55d6-b6c1-d697f5eabc9b';
 $tucker_addr = '17fhrxKpaHHV4r54TbncDPiyB9GEYGkX8f';
 $grant_addr = '1Moyz5FYXLy3LFmER9ya5pruv9VHhpdx64';
